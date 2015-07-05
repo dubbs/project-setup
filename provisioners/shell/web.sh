7f91b492b45e34c969834b1064b2dde9d34ee76e
Kevin Warrington <kevin.warrington@gmail.com>
2015-07-05 16:25:40 -0600

    add vhost

--- provisioners/shell/web.sh
+++ provisioners/shell/web.sh
@@ -1,11 +1,18 @@
 service --status-all|grep -q httpd
+# $? return code from last command
 if [ $? -ne 0 ];then
   # install httpd
   yum -y install httpd
-  # chkconfig --list httpd
-  # httpd          	0:off	1:off	2:off	3:off	4:off	5:off	6:off
   # set runlevels for httpd service
+  #   chkconfig --list httpd
+  #   httpd 0:off 1:off 2:off 3:off 4:off 5:off 6:off
   chkconfig --levels 235 httpd on
+  # enable named virtual hosts
+  sed -i.orig 's/^#NameVirtualHost/NameVirtualHost/' /etc/httpd/conf/httpd.conf
+  # add virtual host entry to conf
+  cat /vagrant/config/httpd/vhosts.conf >> /etc/httpd/conf/httpd.conf
+  # symlink public
+  ln -s /vagrant/public_html /var/www/example.com
   # start httpd
   service httpd start
 fi
