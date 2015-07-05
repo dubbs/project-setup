77ac3748833af569dd60ff5b44e63e091c8f8687
Kevin Warrington <kevin.warrington@gmail.com>
2015-07-05 15:04:24 -0600

    apache up and running

--- provisioners/shell/web.sh
+++ provisioners/shell/web.sh
@@ -2,8 +2,10 @@ service --status-all|grep -q httpd
 if [ $? -ne 0 ];then
   # install httpd
   yum -y install httpd
-  # modify runlevels for httpd service
   # chkconfig --list httpd
   # httpd          	0:off	1:off	2:off	3:off	4:off	5:off	6:off
+  # set runlevels for httpd service
   chkconfig --levels 235 httpd on
+  # start httpd
+  service httpd start
 fi
